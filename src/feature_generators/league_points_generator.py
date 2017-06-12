0119f9b4853cdc5234263dd6c5d23ca540b2765a
KucinyYs <yeshi.shoham@gmail.com>
2017-06-12 16:21:17 +0300

    adding the distance from top feature

--- src/feature_generators/league_points_generator.py
+++ src/feature_generators/league_points_generator.py
@@ -21,17 +21,29 @@ class LeaguePointsGenerator(GeneralGenerator):
         game_list.games_df["LeaguePointsDiff"] = -1
 
         sorted_game_list_df = game_list.games_df.sort_values(by="Date")
+        # holds the number of points for top team per league
+        max_points = {}
+        number_of_games_played = {}
 
         for index, row in sorted_game_list_df.iterrows():
 
             home_team_key = row["HomeTeam"] + row["SeasonId"]
             away_team_key = row["AwayTeam"] + row["SeasonId"]
 
+            if row["SeasonId"] not in max_points:
+                max_points[row["SeasonId"]] = 0
+
             if home_team_key not in season_id_team_points_dic:
                 season_id_team_points_dic[home_team_key] = 0
 
             if away_team_key not in season_id_team_points_dic:
                 season_id_team_points_dic[away_team_key] = 0
+
+            if home_team_key not in number_of_games_played:
+                number_of_games_played[home_team_key] = 0
+
+            if away_team_key not in number_of_games_played:
+                number_of_games_played[away_team_key] = 0
 
             game_list.games_df.loc[int(row.name), "HomeTeamLeaguePoints"] = season_id_team_points_dic[home_team_key]
             game_list.games_df.loc[int(row.name), "AwayTeamLeaguePoints"] = season_id_team_points_dic[away_team_key]
@@ -46,4 +58,23 @@ class LeaguePointsGenerator(GeneralGenerator):
             else:
                 season_id_team_points_dic[away_team_key] += 3
 
+            number_of_games_played[home_team_key] += 1
+            number_of_games_played[away_team_key] += 1
+
+            self.calculate_distance_from_top(away_team_key, game_list, home_team_key, max_points, number_of_games_played,
+                                             row, season_id_team_points_dic)
+
         return game_list
+
+    # here we calculate the average distance from the top of the table, and add it as a feature
+    # note we normalize the distance with the number of games actually played so far
+    def calculate_distance_from_top(self, away_team_key, game_list, home_team_key, max_points, number_of_games_played, row,
+                                    season_id_team_points_dic):
+        # update top points if needed
+        top_team_points = max(season_id_team_points_dic[home_team_key], season_id_team_points_dic[away_team_key])
+        max_points[row["SeasonId"]] = max(top_team_points, max_points[row["SeasonId"]])
+
+        distance_from_top = ((2 * max_points[row["SeasonId"]]) - season_id_team_points_dic[home_team_key] -
+                             season_id_team_points_dic[away_team_key]) / 2
+        normalized_distance_from_top = distance_from_top / number_of_games_played[home_team_key]
+        game_list.games_df.loc[int(row.name), "DistanceFromLeader"] = normalized_distance_from_top
