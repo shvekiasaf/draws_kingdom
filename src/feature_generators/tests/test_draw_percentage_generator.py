be89fd72e58d6b8e3166516dc47cab8d949f2b3e
KucinyYs <yeshi.shoham@gmail.com>
2017-06-18 17:35:42 +0300

    adapting draw percentage to calculate based only on previous games

--- src/feature_generators/tests/test_draw_percentage_generator.py
+++ src/feature_generators/tests/test_draw_percentage_generator.py
@@ -14,11 +14,14 @@ class TestDrawPercentageGenerator(BaseTestCase):
         pass
 
     def test_calculate_game_mamy_games(self):
-        BaseTestCase.clean_cache_files(self, ".*many_games.*dat")
+        BaseTestCase.clean_cache_files(self, ".*draw_perce.*dat")
 
         # Arrange
-        game_list = FootballDataReader.game_list_by_url(url=BaseTestCase.base_url() + "/many_games.csv",
+        game_list = FootballDataReader.game_list_by_url(url=BaseTestCase.base_url() + "/draw_percentage.csv",
                                                         league_name="tests")
         draw_percentage = DrawsPercentageGenerator().calculate_feature(game_list)
-        self.assertEqual(draw_percentage.games_df.loc[3, "DrawPercentage"], 0.5)
-
+        self.assertEqual(draw_percentage.games_df.loc[0, "DrawPercentage"], -1)
+        self.assertEqual(draw_percentage.games_df.loc[1, "DrawPercentage"], 0)
+        self.assertEqual(draw_percentage.games_df.loc[2, "DrawPercentage"], 0)
+        self.assertEqual(draw_percentage.games_df.loc[3, "DrawPercentage"], 1 / 3)
+        self.assertEqual(draw_percentage.games_df.loc[4, "DrawPercentage"], 0.5)
